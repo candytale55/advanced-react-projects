9d87e95786b85d702f906819e3956825858508b0
CandyTale55 <candytale55@gmail.com>
2025-07-31 22:16:50 +0200

    add challenge

--- src/components/login-w-google-button/LoginButtonApp.jsx
+++ src/components/login-w-google-button/LoginButtonApp.jsx
@@ -3,13 +3,15 @@ import Button from "./Button"
 import { FcGoogle } from "react-icons/fc";
 
 /**
- * Challenge: Add an onClick handler to the Button
- * component instance (the one on this page). Then
- * make whatever changes you need for it to actually
- * work.
+ * Challenge: accept the `size` prop and set the `className` of the
+ * <button> to:
  * 
- * Clicking the button should log "Logging in..." 
- * to the console.
+ * "button-small" if `size` is "sm"
+ * "button-large" if `size` is "lg"
+ * 
+ * Note: don't try to manually add a `className` to the Button
+ * instance in index.js yet. (DO add `className` to this file
+ * as part of the challenge, though.)
  */
 
 
