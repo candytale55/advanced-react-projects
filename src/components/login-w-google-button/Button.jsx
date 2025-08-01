cb4d688d3d6784223a0630316ec9de677838fb58
CandyTale55 <candytale55@gmail.com>
2025-08-01 10:36:55 +0200

    add size prop to LoginButtonApp and className on Button.jsx
    - Accept the `size` prop and set the `className` of the <button> to: "button-small" if `size` is "sm" ,"button-large" if `size` is "lg"

--- src/components/login-w-google-button/Button.jsx
+++ src/components/login-w-google-button/Button.jsx
@@ -1,10 +1,31 @@
 import "./assets/styles.css"
 
-export default function Button({children, ...rest}) {
-    console.log(rest)
+export default function Button({children, size, ...rest}) {
+
+    /**
+     * Challenge: accept the `size` prop and set the `className` of the
+     * <button> to:
+     * 
+     * "button-small" if `size` is "sm"
+     * "button-large" if `size` is "lg"
+     * 
+     * Note: don't try to manually add a `className` to the Button
+     * instance in index.js yet. (DO add `className` to this file
+     * as part of the challenge, though.)
+     */
+    
+    //console.log(rest);
+    
+    let className =
+        size === "sm"
+            ? "button-small"
+            : size === "lg"
+                ? "button-large"
+                : "";
+
     return (
         <main>
-            <button {...rest}>
+            <button {...rest} className = {className}>
                 {children}
             </button>
         </main>
--- src/components/login-w-google-button/LoginButtonApp.jsx
+++ src/components/login-w-google-button/LoginButtonApp.jsx
@@ -2,25 +2,17 @@ import "./assets/styles.css"
 import Button from "./Button"
 import { FcGoogle } from "react-icons/fc";
 
-/**
- * Challenge: accept the `size` prop and set the `className` of the
- * <button> to:
- * 
- * "button-small" if `size` is "sm"
- * "button-large" if `size` is "lg"
- * 
- * Note: don't try to manually add a `className` to the Button
- * instance in index.js yet. (DO add `className` to this file
- * as part of the challenge, though.)
- */
-
 
 export default function LoginButtonApp() {
 
     return (
         <main>
             <h2>I am LoginButtonApp</h2>
-            <Button onClick={() => console.log("Logging in...")} style={{color: "darkgreen"}}>
+            <Button
+                onClick={() => console.log("Logging in...")}
+                size="sm"
+            >
+                
                 <FcGoogle />
                 Log in with Google
             </Button>
