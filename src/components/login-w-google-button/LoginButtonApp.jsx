25e2d4e1a52549c7e30f86b6169dfda20f5dc13e
CandyTale55 <candytale55@gmail.com>
2025-08-01 14:37:11 +0200

    test size is working too

--- src/components/login-w-google-button/LoginButtonApp.jsx
+++ src/components/login-w-google-button/LoginButtonApp.jsx
@@ -9,8 +9,8 @@ export default function LoginButtonApp() {
         <main>
             <h2>I am LoginButtonApp</h2>
             <Button><FcGoogle />Log in with Google</Button>
-            <Button variant="success"><FcGoogle />Log in with Google</Button>
-            <Button variant="warning"><FcGoogle />Log in with Google</Button>
+            <Button variant="success" size="sm"><FcGoogle />Log in with Google</Button>
+            <Button variant="warning" size="lg"><FcGoogle />Log in with Google</Button>
             <Button variant="danger"><FcGoogle />Log in with Google</Button>
         </main>
     )
