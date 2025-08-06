05614519955daac705a3ce1895f86e1c3424aa61
CandyTale55 <candytale55@gmail.com>
2025-08-06 12:51:35 +0200

    inject open and toggle props using React.Children.map and React.cloneElement
    Use `React.Children.map` to iterate over `children` and inject `open` and `toggle` props using `React.cloneElement`, so child components can access shared state and behavior.
    
    Inside Menu, it does this:
    1. Iterates over both MenuButton and MenuDropdown
    2. Clones them with open and toggle props. So it returns:
    <MenuButton open={open} toggle={toggle} />
    <MenuDropdown open={open} toggle={toggle} />

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,6 +2,12 @@ import React from "react"
 import MenuButton from "./MenuButton"
 import MenuDropdown from "./MenuDropdown"
 
+/**
+ * Challenge: figure out what needs to be passed to the
+ * children so they'll have access to new props!
+ */
+
+
 export default function Menu({ children }) {
 
     const [open, setOpen] = React.useState(true)
@@ -12,7 +18,12 @@ export default function Menu({ children }) {
 
     return (
         <div className="menu">
-            {children}
+            {React.Children.map(children, (child) => {
+                return React.cloneElement(child, {
+                    open: open,
+                    toggle: toggle
+                })
+            })}
         </div>
     )
 }
--- src/components/Menu/MenuApp.jsx
+++ src/components/Menu/MenuApp.jsx
@@ -4,15 +4,6 @@ import MenuButton from "./MenuButton"
 import MenuDropdown from "./MenuDropdown"
 import MenuItem from "./MenuItem"
 
-/**
- * 1. MenuDropdown should render children instead of items
- * 2. MenuItem (new component you need to create) should also
- *    render children. Grab the <div className="menu-item">
- *    from MenuDropdown before deleting it if you want to be
- *    lazy :)
- * 3. Map over the `sports` array inside MenuDropdown in this
- *    file and render a MenuItem for each sport in the array
- */
 
 export default function MenuApp() {
     let sports = ["Tennis", "Pickleball", "Racquetball", "Squash"]
