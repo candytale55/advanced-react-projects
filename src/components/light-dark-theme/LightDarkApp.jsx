644a6c6f2ad0415824ef1029f185e57b80b899b0
CandyTale55 <candytale55@gmail.com>
2025-08-08 11:39:10 +0200

    add state theme, setTheme to LightDarkApp

--- src/components/light-dark-theme/LightDarkApp.jsx
+++ src/components/light-dark-theme/LightDarkApp.jsx
@@ -3,13 +3,21 @@ import "./assets/styles.css"
 import Header from "./Header"
 import Button from "./Button"
 
+/**
+ * Challenge part 1:
+ * Add state to the App component. Default value === "light". 
+ * Pass that state as the value to the context provider.
+ */
+
 
 const ThemeContext = React.createContext();
 
 export default function LightDarkApp() {
     
+    const [theme, setTheme] = React.useState("dark")
+    
     return (
-        <ThemeContext.Provider value="light">
+        <ThemeContext.Provider value={theme}>
             <div className="container dark-theme">
                 <Header />
                 <Button />
